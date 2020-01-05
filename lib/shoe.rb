821ade040cc7b4e1eb24d7264d975c1e530d0bf1
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-05 22:44:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,6 +14,19 @@ class Shoe
   def size (num)
     @size=num 
   end 
+  def material 
+    @material
+  end 
+  def material=(materials)
+    @material=materials
+  end 
+  def condition 
+    @condition
+  end 
+  def condition=(the_condition)
+    @condition=the_condition
+  end 
+  
   
   
   
